1d764260209a919333cfecd8b6050694ce8ff984
Aaditya Binod Yadav <aadityarayyadav@gmail.com>
2026-02-16 23:05:46 +0545

    final push

---
+++ packages/backend/convex/users.ts
@@ -0,0 +1,24 @@
+import { mutation, query } from "./_generated/server";
+export const getMany = query({
+    args: {},
+    handler: async (ctx) => {
+        const users = await ctx.db.query("users").collect();
+
+        return users;
+    },
+})
+
+export const add = mutation({
+    args: {},
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (identity === null) {
+            throw new Error("Not Authenticated");
+        }
+        const user = await ctx.db.insert("users", {
+            name: "Ari",
+        });
+
+        return user;
+    }
+})
